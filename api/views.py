11825fa9d3529783ce999e33d1234e325acd4157
YashnarbekS <122490631+Programmeryashnar@users.noreply.github.com>
2023-11-20 15:37:20 +0500

    gitlibrary

--- api/views.py
+++ api/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, viewsets
+from rest_framework import viewsets
 from .models import Book, LibUser, RentBook
 from .serializers import BookSerializer, LibUserSerializer, RentBookSerializer
 from rest_framework.response import Response
